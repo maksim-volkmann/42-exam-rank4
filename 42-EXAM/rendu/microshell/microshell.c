0506b813ff244334a5f97dde397d145017590330
Maksim Volkmann <mvolkman@2-H-9.42heilbronn.de>
2024-09-25 16:19:57 +0200

    exam

--- 42-EXAM/rendu/microshell/microshell.c
+++ 42-EXAM/rendu/microshell/microshell.c
@@ -2,65 +2,113 @@
 #include <string.h>
 #include <sys/wait.h>
 
-int err(char *str)
+// Function to print an error message to STDERR
+int print_error(char *msg)
 {
-	while (*str)
-		write(2, str++, 1);
-	return 1;
+    while (*msg)
+        write(2, msg++, 1);
+    return 1;
 }
 
-int cd(char **argv, int i)
+// Function to handle the 'cd' command
+int handle_cd(char **args, int argc)
 {
-	if (i != 2)
-		return err("error: cd: bad arguments\n");
-	if (chdir(argv[1]) == -1)
-		return err("error: cd: cannot change directory to "), err(argv[1]), err("\n");
-	return 0;
+    if (argc != 2)
+        return print_error("error: cd: bad arguments\n");
+    if (chdir(args[1]) == -1)
+    {
+        write(2, "error: cd: cannot change directory to ", 38);
+        write(2, args[1], strlen(args[1]));
+        write(2, "\n", 1);
+        return 1;
+    }
+    return 0;
 }
 
-int exec(char **argv, int i, char **envp)
+// Function to execute a single command, possibly setting up a pipe
+int execute_command(char **args, int argc, char **env)
 {
-	int	fd[2];
-	int	status;
-	int	has_pipe = argv[i] && !strcmp(argv[i], "|");
+    int pipe_fd[2];
+    pid_t pid;
+    int status;
 
-	if (!has_pipe && !strcmp(*argv, "cd"))
-		return cd(argv, i);
+    // Check if the next argument is a pipe
+    int is_pipe = (args[argc] && strcmp(args[argc], "|") == 0);
 
-	if (has_pipe && pipe(fd) == -1)
-		return err("error: fatal\n");
+    // Handle 'cd' if it's a standalone command
+    if (!is_pipe && strcmp(args[0], "cd") == 0)
+        return handle_cd(args, argc);
 
-	int pid = fork();
-	if (!pid)
-	{
-		argv[i] = 0;
-		if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-			return err("error: fatal\n");
-		if (!strcmp(*argv, "cd"))
-			return cd(argv, i);
-		execve(*argv, argv, envp);
-		return err("error: cannot execute "), err(*argv), err("\n");
-	}
+    // If there's a pipe, create it
+    if (is_pipe && pipe(pipe_fd) == -1)
+        return print_error("error: fatal\n");
 
-	waitpid(pid, &status, 0);
-	if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-		return err("error: fatal\n");
-	return WIFEXITED(status) && WEXITSTATUS(status);
+    // Fork the process
+    pid = fork();
+    if (pid == -1)
+        return print_error("error: fatal\n");
+
+    if (pid == 0)
+    {
+        // Child process
+
+        // If there's a pipe, redirect stdout to the pipe's write end
+        if (is_pipe)
+        {
+            if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
+                return print_error("error: fatal\n");
+            close(pipe_fd[0]);
+            close(pipe_fd[1]);
+        }
+
+        // Execute the command
+        execve(args[0], args, env);
+
+        // If execve fails, print an error and exit
+        write(2, "error: cannot execute ", 21);
+        write(2, args[0], strlen(args[0]));
+        write(2, "\n", 1);
+        _exit(1);
+    }
+
+    // Parent process waits for the child to finish
+    waitpid(pid, &status, 0);
+
+    // If there was a pipe, set up the read end for the next command
+    if (is_pipe)
+    {
+        if (dup2(pipe_fd[0], STDIN_FILENO) == -1)
+            return print_error("error: fatal\n");
+        close(pipe_fd[0]);
+        close(pipe_fd[1]);
+    }
+
+    // Return the child's exit status
+    if (WIFEXITED(status))
+        return WEXITSTATUS(status);
+    return 1;
 }
 
-int main(int ac, char **av, char **envp)
+int main(int argc, char **argv, char **env)
 {
-	int	i = 1;
-	int	status = 0;
+    int i = 1;          // Start from 1 to skip the program name
+    int status = 0;     // To store the status of the last executed command
 
-	while (i < ac)
-	{
-		int j = i;
-		while (j < ac && strcmp(av[j], "|") && strcmp(av[j], ";"))
-			j++;
-		if (j > i)
-			status = exec(av + i, j - i, envp);
-		i = j + 1;
-	}
-	return status;
+    while (i < argc)
+    {
+        int j = i;
+
+        // Find the next separator ('|' or ';') or the end of arguments
+        while (j < argc && strcmp(argv[j], "|") && strcmp(argv[j], ";"))
+            j++;
+
+        // Execute the command from argv[i] to argv[j-1] if there are arguments
+        if (j > i)
+            status = execute_command(&argv[i], j - i, env);
+
+        // Move to the next argument after the separator
+        i = j + 1;
+    }
+
+    return status;  // Return the exit status of the last command
 }
--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 
@@ -9,37 +9,35 @@ int err(char *str)
 	return 1;
 }
 
-int cd(char **argv, int i)
+int cd(char **av, int i)
 {
 	if (i != 2)
 		return err("error: cd: bad arguments\n");
-	if (chdir(argv[1]) == -1)
-		return err("error: cd: cannot change directory to "), err(argv[1]), err("\n");
-	return 0;
+	if (chdir(av[1]) == -1)
+		return err("error: cd: cannot change directory to "), err(av[1]), err("\n");
+	return (0);
 }
 
-int execute(char **argv, int i, char **envp)
+int execute(char **av, int i, char **env)
 {
-	int	fd[2];
-	int	status;
-	int	has_pipe = argv[i] && !strcmp(argv[i], "|");
+	int fd[2];
+	int status;
+	int has_pipe = av[i] && !strcmp(av[i], "|");
 
-	if (!has_pipe && !strcmp(*argv, "cd"))
-		return cd(argv, i);
-
-	if (has_pipe && pipe(fd) == -1)
+	if(!has_pipe && !strcmp(*av, "cd"))
+		return cd(av, i);
+	if(has_pipe && pipe(fd) == -1)
 		return err("error: fatal\n");
-
 	int pid = fork();
-	if (!pid)
+	if(pid == -1)
+		return err("error: fatal\n");
+	if(!pid)
 	{
-		argv[i] = 0;
+		av[i] = 0;
 		if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
 			return err("error: fatal\n");
-		if (!strcmp(*argv, "cd"))
-			return cd(argv, i);
-		execve(*argv, argv, envp);
-		return err("error: cannot execute "), err(*argv), err("\n");
+		execve(*av, av, env);
+		return err("error: cannot execute "), err(*av), err("\n");
 	}
 
 	waitpid(pid, &status, 0);
@@ -48,39 +46,19 @@ int execute(char **argv, int i, char **envp)
 	return WIFEXITED(status) && WEXITSTATUS(status);
 }
 
-int main(int ac, char **av, char **envp)
+int main(int ac, char **av, char **env)
 {
-	int	i = 1;
-	int	status = 0;
+	int i = 1;
+	int status = 0;
 
-	while (i < ac)
+	while(i < ac)
 	{
 		int j = i;
-		while (j < ac && strcmp(av[j], "|") && strcmp(av[j], ";"))
+		while(j < ac && strcmp(av[j], "|") && strcmp(av[j], ";"))
 			j++;
-		if (j > i)
-			status = execute(av + i, j - i, envp);
+		if(j > i)
+			status = execute(av + i, j - i, env);
 		i = j + 1;
 	}
 	return status;
 }
-
-// int main(int argc, char **argv, char **envp)
-// {
-// 	int i = 0;
-// 	int status = 0;
-
-// 	if (argc > 1)
-// 	{
-// 		while (argv[i] && argv[++i])
-// 		{
-// 			argv += i;
-// 			i = 0;
-// 			while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
-// 				i++;
-// 			if (i)
-// 				status = execute(argv, i, envp);
-// 		}
-// 	}
-// 	return status;
-// }
